4038230d4eba86f4e09ad276b3ff8ea4f4861495
Demonju <jake.zammit.d57298@mcast.edu.mt>
2024-01-04 23:56:48 +0100

    Created MainMenu/PauseMenu
    Finished the Main Menu as well as Pause Menu you can seamlessly switch between levels as well as pause and resume within each level

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
